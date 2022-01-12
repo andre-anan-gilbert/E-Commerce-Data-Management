888f011ac3bc2ee4e1a5bfc2a5ae547e98b6951f
André Anan Gilbert <andre.gilbert.77110@gmail.com>
2022-01-12 11:18:16 +0100

    added sqlalchemy

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,5 +1,4 @@
 """REST API."""
-import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -8,7 +7,3 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"message": "Hello World"}
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
