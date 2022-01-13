4af4ce924349a2863e1358bf6c100277e1d9b3b4
André-Anan Gilbert <andre.gilbert.77110@gmail.com>
2022-01-13 09:34:36 +0100

    Update base.py

--- backend/api/database/base.py
+++ backend/api/database/base.py
@@ -1,2 +1,2 @@
-"""Base."""
+"""Import all the models, so that Base has them before being imported by Alembic (Base.metadata)."""
 from api.database.session import Base  # pylint: disable=unused-import
