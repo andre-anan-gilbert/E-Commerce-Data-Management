2d0acfb29f621a62c23bd8c68c46acea2fbdaafb
André Anan Gilbert <andre.gilbert.77110@gmail.com>
2022-01-13 10:29:10 +0100

    added routers, middleware

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,9 +1,25 @@
 """REST API."""
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from api.routers import item
 
 app = FastAPI()
 
+# Middleware
+origins = ['*']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
-@app.get("/")
+# Routers
+app.include_router(item.router)
+
+
+@app.get('/')
 async def read_root():
-    return {"message": "Hello World"}
+    return {'message': 'Hello World'}
--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -6,6 +6,6 @@ client = TestClient(app)
 
 
 def test_read_main():
-    response = client.get("/")
+    response = client.get('/')
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    assert response.json() == {'message': 'Hello World'}
