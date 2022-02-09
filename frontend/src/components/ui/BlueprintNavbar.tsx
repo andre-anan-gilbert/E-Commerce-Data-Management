525ea91b1c8f4a7fc56b4267bb42f964d0309d6e
André Anan Gilbert <andre.gilbert.77110@gmail.com>
2022-02-09 11:52:55 +0100

    updated navbar

--- frontend/src/components/ui/BlueprintNavbar.tsx
+++ frontend/src/components/ui/BlueprintNavbar.tsx
@@ -11,11 +11,19 @@ import {
 const BlueprintNavbar = () => {
   return (
     <Navbar>
-      <NavbarGroup align="left">
-        <NavbarHeading>Blueprint</NavbarHeading>
-        <NavbarDivider />
-        <Button className={Classes.MINIMAL} icon="home" text="Home" />
+      <NavbarGroup>
+        <NavbarHeading>Blueprint Components</NavbarHeading>
+        <input className="pt-input" placeholder="Search files..." type="text" />
+      </NavbarGroup>
+      <NavbarGroup align="right">
+        <Button className={Classes.MINIMAL} icon="home">
+          Home
+        </Button>
         <Button className={Classes.MINIMAL} icon="document" text="Files" />
+        <NavbarDivider />
+        <Button className={Classes.MINIMAL} icon="user" />
+        <Button className={Classes.MINIMAL} icon="notifications" />
+        <Button className={Classes.MINIMAL} icon="cog" />
       </NavbarGroup>
     </Navbar>
   );
