a3ee0ea9b878d46c78883a72d095569ee7ec9018
André Anan Gilbert <andre.gilbert.77110@gmail.com>
2022-03-02 16:42:38 +0100

    added redux store

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,8 @@
-import type { ReactElement, ReactNode } from "react";
-import type { NextPage } from "next";
-import type { AppProps } from "next/app";
+import { ReactElement, ReactNode } from "react";
+import { NextPage } from "next";
+import { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { store } from "../redux/store";
 import { GlobalStyle } from "../styles/global-style";
 
 type NextPageWithLayout = NextPage & {
@@ -14,10 +16,10 @@ type AppPropsWithLayout = AppProps & {
 function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyle />
       {getLayout(<Component {...pageProps} />)}
-    </>
+    </Provider>
   );
 }
 
