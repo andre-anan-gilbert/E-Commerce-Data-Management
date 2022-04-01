c173971b22687475b8a987499b0e3e23746fcbe9
André Anan Gilbert <andre.gilbert.77110@gmail.com>
2022-04-01 20:12:58 +0200

    test fastapi workflow #6

--- backend/app/tests/api/api_v1/test_products.py
+++ backend/app/tests/api/api_v1/test_products.py
@@ -4,6 +4,6 @@ from app.core.config import settings
 
 
 def test_products(client: TestClient) -> None:
-    response = client.get(f'{settings.API_V1_STR}/products')
+    response = client.get(f'{settings.API_V1_STR}/products/')
     assert response.status_code == 200
     assert response.json() == {'message': 'Hello World'}
