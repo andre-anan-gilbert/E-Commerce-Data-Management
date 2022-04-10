cf5475a7a38c5c0e336910cae9bdd004f71dd49d
André Anan Gilbert <andre.gilbert.77110@gmail.com>
2022-04-10 16:10:06 +0200

    added navbar & footer

--- frontend/src/components/ui/Footer.tsx
+++ frontend/src/components/ui/Footer.tsx
@@ -1,7 +1,36 @@
 /** The footer of the application. */
+import styled from 'styled-components';
+import { Classes, Icon } from '@blueprintjs/core';
 
-const Footer = () => {
-  return <div>Footer</div>;
+export const Footer = () => {
+  return (
+    <Foo>
+      <Copyright className={Classes.TEXT_MUTED}>
+        Copyright © 2022 404, Inc. All rights reserved.
+      </Copyright>
+      <a
+        href="https://github.com/Andre-Gilbert/Database"
+        className={Classes.TEXT_MUTED}
+      >
+        <Icon icon="git-repo" />
+      </a>
+    </Foo>
+  );
 };
 
-export default Footer;
+const Foo = styled.div`
+  width: min(100%, 120rem);
+  min-height: 5rem;
+  margin-inline: auto;
+  padding: 0 1.2rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  background-color: ${({ theme }) => theme.text.light};
+`;
+
+const Copyright = styled.p`
+  margin: 0;
+`;
--- frontend/src/components/ui/Navbar.tsx
+++ frontend/src/components/ui/Navbar.tsx
@@ -1,17 +1,68 @@
 /** The navbar of the landing page. */
 import styled from 'styled-components';
+import Image from 'next/image';
+import Logo from '../../images/logo.svg';
 
-const Navbar = () => {
-  return <Nav>Navbar</Nav>;
+export const Navbar = () => {
+  return (
+    <Header>
+      <Wrapper>
+        <Image src={Logo} alt="logo" />
+        <ButtonWrapper>
+          <SignInButton type="button">Sign in</SignInButton>
+          <SignUpButton type="button">Sign up</SignUpButton>
+        </ButtonWrapper>
+      </Wrapper>
+    </Header>
+  );
 };
 
-export default Navbar;
+const Header = styled.header`
+  position: absolute;
+  left: 0;
+  right: 0;
+  z-index: 100;
+`;
 
-const Nav = styled.div`
-  width: 100%;
-  min-height: 5.6rem;
-  padding: 0 1.2rem;
+const Wrapper = styled.div`
+  width: min(100%, 120rem);
+  min-height: 5rem;
+  margin-inline: auto;
+  padding-inline: 0.8rem;
 
   display: flex;
   align-items: center;
+  justify-content: space-between;
+`;
+
+const ButtonWrapper = styled.div`
+  display: inline-block;
+`;
+
+const Button = styled.button`
+  position: relative;
+  display: inline-flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  font-weight: 500;
+  border: none;
+  white-space: nowrap;
+`;
+
+const SignInButton = styled(Button)`
+  color: ${({ theme }) => theme.text.light};
+  background-color: transparent;
+`;
+
+const SignUpButton = styled(Button)`
+  padding: 0.8rem 1.6rem;
+  margin-left: 1.6rem;
+
+  color: ${({ theme }) => theme.text.light};
+  background-color: transparent;
+
+  border: 1px solid ${({ theme }) => theme.text.light};
+  border-radius: 0.6rem;
 `;
