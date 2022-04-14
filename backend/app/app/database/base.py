ab0bd9f280592a456b8af07bbffd4305c70b0ba2
Valentin Müller <valentinmoritzmueller@gmail.com>
2022-04-14 15:09:48 +0200

    added missing model to base file

--- backend/app/app/database/base.py
+++ backend/app/app/database/base.py
@@ -5,13 +5,14 @@ from app.database.session import Base
 from app.models.address_2_customer import Address2Customer
 from app.models.address import Address
 from app.models.category import Category
+from app.models.customer import Customer
 from app.models.department import Department
 from app.models.employee import Employee
 from app.models.invoice import Invoice
 from app.models.order_detail import OrderDetail
 from app.models.order import Order
-from app.models.payment_information import PaymentInformation
 from app.models.payment_information_2_customer import PaymentInformation2Customer
+from app.models.payment_information import PaymentInformation
 from app.models.postal_code_2_city import PostalCode2City
 from app.models.product_2_warehouse import Product2Warehouse
 from app.models.product import Product
