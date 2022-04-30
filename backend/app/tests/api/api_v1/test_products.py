2af6cd376783df2f988e29caa02925f3c158562e
André Anan Gilbert <andre.gilbert.77110@gmail.com>
2022-04-30 17:33:08 +0200

    test pytest workflow

--- backend/app/tests/api/api_v1/test_products.py
+++ backend/app/tests/api/api_v1/test_products.py
@@ -2,6 +2,7 @@
 from fastapi.testclient import TestClient
 from app.core.config import settings
 
+
 def test_products(client: TestClient) -> None:
     response = client.get(f'{settings.API_V1_STR}/products/')
     assert response.status_code == 200
