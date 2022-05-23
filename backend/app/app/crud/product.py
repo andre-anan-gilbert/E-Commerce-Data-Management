45de8d8eafad23d73f89786d4a64b2ba2e4037f7
Valentin Müller <valentinmoritzmueller@gmail.com>
2022-05-23 14:28:46 +0200

    fixed code smells

--- backend/app/app/crud/product.py
+++ backend/app/app/crud/product.py
@@ -1,8 +1,3 @@
-from typing import List
-
-from fastapi.encoders import jsonable_encoder
-from sqlalchemy.orm import Session
-
 from app.crud.base import CRUDBase
 from app.models.product import Product
 from app.schemas.product import ProductCreate, ProductUpdate
