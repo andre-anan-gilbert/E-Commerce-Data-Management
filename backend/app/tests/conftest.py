e77fe0243a6e33433bdf87ed300d5e882ecf02b5
Valentin Müller <valentinmoritzmueller@gmail.com>
2022-06-05 11:25:50 +0200

    fixed conftest

--- backend/app/tests/conftest.py
+++ backend/app/tests/conftest.py
@@ -20,5 +20,5 @@ def client() -> Generator:
 
 
 @pytest.fixture(scope='module')
-def token_headers(test_client: TestClient) -> Dict[str, str]:
-    return get_token_headers(test_client)
+def token_headers(client: TestClient) -> Dict[str, str]:  # pylint: disable=redefined-outer-name
+    return get_token_headers(client)
