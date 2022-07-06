5246d800af20e53cb4eb6ec69a83d0da3404b67e
André Anan Gilbert <andre.gilbert.77110@gmail.com>
2022-07-06 19:52:03 +0200

    updated sign in and sign up tests

--- frontend/cypress/integration/sign-in.spec.ts
+++ frontend/cypress/integration/sign-in.spec.ts
@@ -6,9 +6,22 @@ describe('Sign in', () => {
   });
 
   it('Fill out sign in form', () => {
+    cy.intercept(
+      {
+        method: 'POST',
+        url: '**/user/sign-in',
+      },
+      { fixture: 'access-token' }
+    ).as('accessToken');
     cy.get('[data-cy="sign-in"]').click();
-    cy.get('[data-cy="sign-in-email"]').type('test@user.com');
-    cy.get('[data-cy="sign-in-password"]').type('password123');
+    cy.get('[data-cy="sign-in-email"]')
+      .should('have.value', '')
+      .type('test@user.com')
+      .should('have.value', 'test@user.com');
+    cy.get('[data-cy="sign-in-password"]')
+      .should('have.value', '')
+      .type('password123')
+      .should('have.value', 'password123');
     cy.get('[data-cy="sign-in-submit"]').click();
   });
 });
--- frontend/cypress/integration/sign-up.spec.ts
+++ frontend/cypress/integration/sign-up.spec.ts
@@ -6,10 +6,26 @@ describe('Sign up', () => {
   });
 
   it('Fill out sign up form', () => {
+    cy.intercept(
+      {
+        method: 'POST',
+        url: '**/user/sign-up',
+      },
+      { fixture: 'access-token' }
+    ).as('accessToken');
     cy.get('[data-cy="sign-up"]').click();
-    cy.get('[data-cy="sign-up-email"]').type('test@user.com');
-    cy.get('[data-cy="sign-up-password"]').type('password123');
-    cy.get('[data-cy="sign-up-confirm-password"]').type('password123');
+    cy.get('[data-cy="sign-up-email"]')
+      .should('have.value', '')
+      .type('test@user.com')
+      .should('have.value', 'test@user.com');
+    cy.get('[data-cy="sign-up-password"]')
+      .should('have.value', '')
+      .type('password123')
+      .should('have.value', 'password123');
+    cy.get('[data-cy="sign-up-confirm-password"]')
+      .should('have.value', '')
+      .type('password123')
+      .should('have.value', 'password123');
     cy.get('[data-cy="sign-up-submit"]').click();
   });
 });
